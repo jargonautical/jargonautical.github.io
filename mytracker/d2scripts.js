f4f0038a1b918b8edd7477e6122ae80d1aa626c3
Lucy Knight <lucyknight@Lucys-MacBook-Air-2.local>
2022-02-10 16:03:21 +0000

    trying to make fetch happen again

--- mytracker/d2scripts.js
+++ mytracker/d2scripts.js
@@ -13,12 +13,17 @@ var apiKey = 'a43fee90ad014eb880cb591885a383c4'
 
 console.log(apiKey);
 
-var xhr = new XMLHttpRequest();
+//var xhr = new XMLHttpRequest();
 //xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
 //xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009360795669/Stats/", true);
 //xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009423934835/Stats/Activities/?mode=5", true);
-
-fetch('https://jsonplaceholder.typicode.com/users')
+var url = 'https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009423934835/Stats/Activities/?mode=5'
+//url = 'https://jsonplaceholder.typicode.com/users'
+fetch(url), {
+  credentials: 'include',
+  headers: {
+    'Authorization': apiKey
+  }
   .then(response => {
     return response.json();
   })
