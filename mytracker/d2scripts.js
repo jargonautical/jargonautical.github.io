9b9c17fb8ddb2fa022b6964fcf08f34db6b13e02
Lucy Knight <lucyknight@Lucys-MacBook-Air-2.local>
2022-02-10 15:09:32 +0000

    tweaks5

--- mytracker/d2scripts.js
+++ mytracker/d2scripts.js
@@ -11,6 +11,8 @@ app.appendChild(container);
 
 var apiKey = 'a43fee90ad014eb880cb591885a383c4'
 
+console.log(apiKey);
+
 var xhr = new XMLHttpRequest();
 //xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
 //xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009360795669/Stats/", true);
@@ -22,7 +24,7 @@ xhr.setRequestHeader("X-API-Key", apiKey);
 
 xhr.onreadystatechange = function(){
   var json = JSON.parse(this.responseText);
-	console.log(json.Response);
+	console.log(json);
   //console.log(json.Response.data.inventoryItem); //Gjallarhorn
  }
 
