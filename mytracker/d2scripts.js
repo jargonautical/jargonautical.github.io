07ab201bb1147c5e78386718b46f255ca6b420d6
Lucy Knight <lucyknight@Lucys-MacBook-Air-2.local>
2022-02-10 17:46:57 +0000

    trying to make fetch happen again 5

--- mytracker/d2scripts.js
+++ mytracker/d2scripts.js
@@ -10,27 +10,16 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var apiKey = 'a43fee90ad014eb880cb591885a383c4'
-
 console.log(apiKey);
 
-//var xhr = new XMLHttpRequest();
-//xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
-//xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009360795669/Stats/", true);
-//xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009423934835/Stats/Activities/?mode=5", true);
-var url = 'https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009423934835/Stats/Activities/?mode=5'
-//url = 'https://jsonplaceholder.typicode.com/users'
-fetch(url, {
-  credentials: 'include',
+$.ajax({
+  url: "https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009360795669/Stats/?daystart=2020-01-01&dayend=2020-01-31&mode=63&periodType=1/",
+  //url: "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/",
   headers: {
-    'X-API-Key': apiKey
+    "X-API-Key": apiKey
   }
-}
-).then(
-  response => {
-    return response.json();
-  }
-).then(
-  users => {
-    console.log(users);
-  }
-)
+}).done(function(json){
+  //console.log(json.Response.data.inventoryItem.itemName); //Gjallarhorn
+  console.log('is this even working?');
+  //console.log(json.Response); //data
+});
