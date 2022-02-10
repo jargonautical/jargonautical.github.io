47cc427a9fdf24bb90175d71ffa3292f2529513f
Lucy Knight <lucyknight@Lucys-MacBook-Air-2.local>
2022-02-10 15:54:58 +0000

    trying to make fetch happen

--- mytracker/d2scripts.js
+++ mytracker/d2scripts.js
@@ -16,16 +16,12 @@ console.log(apiKey);
 var xhr = new XMLHttpRequest();
 //xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
 //xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009360795669/Stats/", true);
-xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009423934835/Stats/Activities/?mode=5", true);
+//xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/3/Account/4611686018476645284/Character/2305843009423934835/Stats/Activities/?mode=5", true);
 
-
-
-xhr.setRequestHeader("X-API-Key", apiKey);
-
-xhr.onreadystatechange = function(){
-  var json = JSON.parse(this.responseText);
-	console.log(json);
-  //console.log(json.Response.data.inventoryItem); //Gjallarhorn
- }
-
-xhr.send();
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    return response.json();
+  })
+  .then(users => {
+    console.log(users);
+  });
