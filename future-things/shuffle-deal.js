9c6d0d36a4d9934056d0915ff0a316832909fea4
Lucy Knight <lucyknight@Lucys-MacBook-Air-2.local>
2022-06-29 16:58:42 +0100

    bug fix

--- future-things/shuffle-deal.js
+++ future-things/shuffle-deal.js
@@ -2,50 +2,81 @@ const app = document.getElementById('root');
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 app.appendChild(container);
-const showCard = document.createElement('div');
-showCard.setAttribute('class', 'card');
-container.appendChild(showCard);
+
+const showCard1 = document.createElement('div');
+showCard1.setAttribute('class', 'card');
+container.appendChild(showCard1);
+
+const showCard2 = document.createElement('div');
+showCard2.setAttribute('class', 'card');
+container.appendChild(showCard2);
+
+const showCard3 = document.createElement('div');
+showCard3.setAttribute('class', 'card');
+container.appendChild(showCard3);
+
+const showCard4 = document.createElement('div');
+showCard4.setAttribute('class', 'card');
+container.appendChild(showCard4);
 
 // program to shuffle the deck of cards
 
-// declare card elements
-const suits = ["Spades", "Diamonds", "Club", "Heart"];
-const values = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King",];
-
-// empty array to contain cards
-let deck = [];
-
-// create a deck of cards
-for (let i = 0; i < suits.length; i++) {
-    for (let x = 0; x < values.length; x++) {
-        let card = { Value: values[x], Suit: suits[i] };
-        deck.push(card);
-    }
-}
-
-// shuffle the cards
-for (let i = deck.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i);
-    let temp = deck[i];
-    deck[i] = deck[j];
-    deck[j] = temp;
-}
-
-console.log('The first five cards are:');
-// display 5 results
-for (let i = 0; i < 5; i++) {
-    console.log(`${deck[i].Value} of ${deck[i].Suit}`)
-}
-
-
-
-const foo = document.createElement('p');
-foo.textContent = 'Cards go here';
-showCard.appendChild(foo);
-
-for (let i = 0; i < 5; i++) {
-  const thisCard = document.createElement('p');
-  thisCard.textContent = `${deck[i].Value} of ${deck[i].Suit}`;
-  console.log('function working? ', thisCard);
-  showCard.appendChild(thisCard);
+function shuffle(thisDeck) {
+  // shuffle the cards
+  for (let i = thisDeck.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      let temp = thisDeck[i];
+      thisDeck[i] = thisDeck[j];
+      thisDeck[j] = temp;
+      //console.log('your card is: ', temp);
+      return temp;
   }
+}
+
+let arcCards = [];
+function getArcCards() {
+  // declare card elements
+  const suits = ["collapse", "discipline", "grow", "transform"];
+  const values = ["aCentury","aDecade","aFewYears","aGeneration","aMillenium","twoGenerations",];
+  // create arc cards
+  for (let i = 0; i < suits.length; i++) {
+      for (let x = 0; x < values.length; x++) {
+          let card = { Value: values[x], Suit: suits[i] };
+          arcCards.push(card);
+      }
+  }
+  console.log('arcCards deck: ', arcCards);
+}
+
+let moodCards = [];
+function getMoodCards() {
+  // declare card elements
+  const suits = ["mood"];
+  const values = ["admiration","alienation","amusement","anger","anxiety","awkwardness",];
+  // create mood cards
+  for (let i = 0; i < suits.length; i++) {
+      for (let x = 0; x < values.length; x++) {
+          let card = { Value: values[x], Suit: suits[i] };
+          moodCards.push(card);
+      }
+  }
+  console.log('moodCards deck: ', moodCards);
+}
+
+getArcCards();
+getMoodCards();
+
+var arcCard = shuffle(arcCards);
+var moodCard = shuffle(moodCards);
+console.log(arcCard, moodCard);
+
+// build arc card container
+const placeArcCard = document.createElement('p');
+placeArcCard.textContent = `arc_${arcCard.Suit}_${arcCard.Value}.png`;
+//console.log('function working? ', thisCard);
+showCard1.appendChild(placeArcCard);
+
+// build arc card container
+const placeMoodCard = document.createElement('p');
+placeMoodCard.textContent = `${moodCard.Suit}_${moodCard.Value}.png`;
+showCard2.appendChild(placeMoodCard);
