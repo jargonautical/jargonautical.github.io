91d1e4d8727146d4e46546c0ba8ae7ab3c646f0e
Lucy Knight <lucyknight@Lucys-MacBook-Air-2.local>
2022-06-29 20:51:04 +0100

    ttftf update

--- future-things/shuffle-deal.js
+++ future-things/shuffle-deal.js
@@ -28,6 +28,7 @@ function shuffle(thisDeck) {
       let temp = thisDeck[i];
       thisDeck[i] = thisDeck[j];
       thisDeck[j] = temp;
+      console.log(i,j);
       //console.log('your card is: ', temp);
       return temp;
   }
@@ -36,8 +37,8 @@ function shuffle(thisDeck) {
 let arcCards = [];
 function getArcCards() {
   // declare card elements
-  const suits = ["collapse", "discipline", "grow", "transform"];
-  const values = ["aCentury","aDecade","aFewYears","aGeneration","aMillenium","twoGenerations",];
+  const suits = ["arc_collapse", "arc_discipline", "arc_grow", "arc_transform"];
+  const values = ["aCentury","aDecade","aFewYears","aGeneration","aMillennium","twoGenerations",];
   // create arc cards
   for (let i = 0; i < suits.length; i++) {
       for (let x = 0; x < values.length; x++) {
@@ -45,14 +46,25 @@ function getArcCards() {
           arcCards.push(card);
       }
   }
-  console.log('arcCards deck: ', arcCards);
+  //console.log('arcCards deck: ', arcCards);
 }
 
 let moodCards = [];
 function getMoodCards() {
   // declare card elements
   const suits = ["mood"];
-  const values = ["admiration","alienation","amusement","anger","anxiety","awkwardness",];
+  const values = [
+    "admiration","alienation","amusement","anger","anxiety","awkwardness",
+    "calm","charm","cheer","contentment","curiosity",
+    "decadence","delight","dignity","disgust","dread",
+    "embarrassment","excitement","exhilaration",
+    "fascination","fervor","frustration",
+    "gratitude","happiness","hilarity","hope","longing",
+    "malaise","melancholy","melodrama","nostalgia","optimism","outrage",
+    "pathos","pleasure","pride","rationality","relief","resentment","respect",
+    "sadness","satisfaction","serenity","shame","shock","sorrow","surprise",
+    "unease","warmth","weirdness","wellbeing","wonder","worry","zen",
+  ];
   // create mood cards
   for (let i = 0; i < suits.length; i++) {
       for (let x = 0; x < values.length; x++) {
@@ -60,23 +72,110 @@ function getMoodCards() {
           moodCards.push(card);
       }
   }
-  console.log('moodCards deck: ', moodCards);
+  //console.log('moodCards deck: ', moodCards);
+}
+
+let objectCards = [];
+function getObjectCards() {
+  // declare card elements
+  const suits = ["object"];
+  const values = [
+    "advertisement","artwork","beverage","book","bottle","box","brochure",
+    "building","candy","clothing","corporation","device","document","event",
+    "festival","flag","game","gift","headline","implant","instrument",
+    "jewellery","kit","law","logo","lotion","machine","magazineCover","map",
+    "mask","monument","passport","pill","plant","postcard","poster","product",
+    "prosthetic","publicServiceAnnouncement","relic","ritual","show","slogan",
+    "snack","song","souvenir","statue","sticker","symbol","tattoo","tool","toy",
+    "tShirt","vehicle","video","weapon","wildcard",
+  ];
+  // create object cards
+  for (let i = 0; i < suits.length; i++) {
+      for (let x = 0; x < values.length; x++) {
+          let card = { Value: values[x], Suit: suits[i] };
+          objectCards.push(card);
+      }
+  }
+  //console.log('objectCards deck: ', objectCards);
+}
+
+let terrainCards = [];
+function getTerrainCards() {
+  // declare card elements
+  const suits = ["terrain"];
+  const values = [
+    "agriculture","childhood","citizenship","class","climate","cloning",
+    "communications","court","disease","drones","education","entertainment",
+    "environment","equality","family","fashion","flight","forests","gender",
+    "genetics","governance","health","hobbies","home","identity","insects",
+    "intellectualProperty","journalism","justice","learning","memory","mining",
+    "music","oceans","oil","oldAge","pets","power","religion","robots","sex",
+    "shopping","space","sports","theatre","theBrain","theEconomy","theMoon",
+    "theZoo","travel","war","water","wealth","wildcard","woman","work","zombies",
+  ];
+  // create terrain cards
+  for (let i = 0; i < suits.length; i++) {
+      for (let x = 0; x < values.length; x++) {
+          let card = { Value: values[x], Suit: suits[i] };
+          terrainCards.push(card);
+      }
+  }
+  //console.log('terrainCards deck: ', terrainCards);
 }
 
 getArcCards();
 getMoodCards();
+getObjectCards();
+getTerrainCards();
 
-var arcCard = shuffle(arcCards);
-var moodCard = shuffle(moodCards);
-console.log(arcCard, moodCard);
+const arcWild = document.createElement('img');
+arcWild.setAttribute("src", "cards/arc_collapse_aCentury.png");
+showCard1.appendChild(arcWild);
 
-// build arc card container
-const placeArcCard = document.createElement('p');
-placeArcCard.textContent = `arc_${arcCard.Suit}_${arcCard.Value}.png`;
-//console.log('function working? ', thisCard);
-showCard1.appendChild(placeArcCard);
+const moodWild = document.createElement('img');
+moodWild.setAttribute("src", "cards/mood_zen.png");
+showCard2.appendChild(moodWild);
 
-// build arc card container
-const placeMoodCard = document.createElement('p');
-placeMoodCard.textContent = `${moodCard.Suit}_${moodCard.Value}.png`;
-showCard2.appendChild(placeMoodCard);
+const objectWild = document.createElement('img');
+objectWild.setAttribute("src", "cards/object_wildcard.png");
+showCard3.appendChild(objectWild);
+
+const terrainWild = document.createElement('img');
+terrainWild.setAttribute("src","cards/terrain_wildcard.png");
+showCard4.appendChild(terrainWild);
+
+function dealCards() {
+  var arcCard = shuffle(arcCards);
+  var moodCard = shuffle(moodCards);
+  var objectCard = shuffle(objectCards);
+  var terrainCard = shuffle(terrainCards);
+  console.log(arcCard, moodCard, objectCard, terrainCard);
+
+  // build arc card container
+  const placeArcCard = document.createElement('img');
+  var arcPicSrc = `cards/${arcCard.Suit}_${arcCard.Value}.png`;
+  console.log(arcPicSrc);
+  placeArcCard.setAttribute("src", arcPicSrc);
+  showCard1.replaceChildren(placeArcCard);
+
+  // build mood card container
+  const placeMoodCard = document.createElement('img');
+  var moodPicSrc = `cards/${moodCard.Suit}_${moodCard.Value}.png`;
+  console.log(moodPicSrc);
+  placeMoodCard.setAttribute("src", moodPicSrc);
+  showCard2.replaceChildren(placeMoodCard);
+
+  // build object card container
+  const placeObjectCard = document.createElement('img');
+  var objectPicSrc = `cards/${objectCard.Suit}_${objectCard.Value}.png`;
+  console.log(objectPicSrc);
+  placeObjectCard.setAttribute("src", objectPicSrc);
+  showCard3.replaceChildren(placeObjectCard);
+
+  // build terrain card container
+  const placeTerrainCard = document.createElement('img');
+  var terrainPicSrc = `cards/${terrainCard.Suit}_${terrainCard.Value}.png`;
+  console.log(terrainPicSrc);
+  placeTerrainCard.setAttribute("src", terrainPicSrc);
+  showCard4.replaceChildren(placeTerrainCard);
+}
